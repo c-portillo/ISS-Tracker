0df2cbd21450cade9474a55a2ad7b375814e9fdf
Carlos Portillo <c.portillojr96@gmail.com>
2020-11-13 03:11:53 -0500

    Switch to UTC time and only extract the hour

--- main.py
+++ main.py
@@ -20,20 +20,22 @@ longitude = ISS_data['iss_position']['longitude']
 parameters = {
     'lat': MY_LATITUDE,
     'lng': MY_LONGITUDE,
-    'formatted': 0
+    'formatted': 0  # switch formatting off to get 24-hour time
 }
 sunrise_sunset_response = requests.get(url='https://api.sunrise-sunset.org/json', params=parameters)
 sunrise_sunset_response.raise_for_status()
 
 # parse sunrise/sunset data - NOTE: all times are in UTC
 sunrise_sunset_data = sunrise_sunset_response.json()
-sunrise = sunrise_sunset_data['results']['sunrise']
+sunrise = sunrise_sunset_data['results']['sunrise'].split('T')[1].split(':')[0]
+sunset = sunrise_sunset_data['results']['sunset'].split('T')[1].split(':')[0]
 
 # get current time
-time_now = datetime.now()
+time_now = datetime.utcnow().hour
 
 # test
 print(f'sunrise: {sunrise}')
+print(f'sunset: {sunset}')
 print(f'time now: {time_now}')
 print(f'ISS latitude: {latitude}')
 print(f'ISS longitude: {longitude}')
