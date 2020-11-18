bbe21cbaf614cf5814d0cbb5c986514994fc32cf
Carlos Portillo <c.portillojr96@gmail.com>
2020-11-18 03:46:08 -0500

    Refactoring with functions

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
+import os
 import requests
+from dotenv import load_dotenv
 from datetime import datetime
+from twilio.rest import Client
+
+load_dotenv()   # loads .env file in current directory
 
 # set my location
 MY_LATITUDE = 40.712776
 MY_LONGITUDE = -74.005974
 
-# get ISS location data
-ISS_location_response = requests.get(url='http://api.open-notify.org/iss-now.json')
 
-# raise appropriate error if response is not successful
-ISS_location_response.raise_for_status()
+def iss_overhead():
+    """ Returns true if ISS is within 5 degrees of my current location, false otherwise """
 
-# parse ISS location data
-ISS_data = ISS_location_response.json()
-latitude = ISS_data['iss_position']['latitude']
-longitude = ISS_data['iss_position']['longitude']
+    iss_location_response = requests.get(url='http://api.open-notify.org/iss-now.json')
+    iss_location_response.raise_for_status()    # raise error if response is not successful
 
-# get my location's sunrise/sunset time data
-parameters = {
-    'lat': MY_LATITUDE,
-    'lng': MY_LONGITUDE,
-    'formatted': 0  # switch formatting off to get 24-hour time
-}
-sunrise_sunset_response = requests.get(url='https://api.sunrise-sunset.org/json', params=parameters)
-sunrise_sunset_response.raise_for_status()
+    # get current latitude and longitude of ISS
+    iss_data = iss_location_response.json()
+    latitude = float(iss_data['iss_position']['latitude'])
+    longitude = float(iss_data['iss_position']['longitude'])
 
-# parse sunrise/sunset data - NOTE: all times are in UTC
-sunrise_sunset_data = sunrise_sunset_response.json()
-sunrise = sunrise_sunset_data['results']['sunrise'].split('T')[1].split(':')[0]
-sunset = sunrise_sunset_data['results']['sunset'].split('T')[1].split(':')[0]
+    # check if ISS is within 5 degrees of my location
+    if MY_LATITUDE-5 <= latitude <= MY_LATITUDE+5 and MY_LONGITUDE-5 <= longitude <= MY_LONGITUDE+5:
+        return True
 
-# get current time
-time_now = datetime.utcnow().hour
 
-# test
-print(f'sunrise: {sunrise}')
-print(f'sunset: {sunset}')
-print(f'time now: {time_now}')
-print(f'ISS latitude: {latitude}')
-print(f'ISS longitude: {longitude}')
+def is_dark():
+    """ Returns true of it is currently dark in my location, false otherwise """
+
+    parameters = {
+        'lat': MY_LATITUDE,
+        'lng': MY_LONGITUDE,
+        'formatted': 0  # switch formatting off to get 24-hour time
+    }
+    sunrise_sunset_response = requests.get(url='https://api.sunrise-sunset.org/json', params=parameters)
+    sunrise_sunset_response.raise_for_status()  # raise error if response is not successful
+
+    # parse sunrise/sunset data - NOTE: all times are in UTC
+    sunrise_sunset_data = sunrise_sunset_response.json()
+    sunrise = int(sunrise_sunset_data['results']['sunrise'].split('T')[1].split(':')[0])
+    sunset = int(sunrise_sunset_data['results']['sunset'].split('T')[1].split(':')[0])
+
+    time_now = datetime.utcnow().hour
+
+    if time_now <= sunrise or time_now >= sunset:
+        return True
+
+
 
